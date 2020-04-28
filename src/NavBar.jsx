9de6a93243e103372a4a34ca5bc64e6a1f754465
Constantin Alexandru <UnCorrupted_00@protonmail.ch>
2020-04-28 15:33:36 +0300

    resolved material-ui warnings by removing component.

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -7,21 +7,19 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <div className="NavBar">
-      <AppBar position="static">
-        <Tabs centered>
-          <Link className="NavBar-Link" to="/programare">
-            Programare
-          </Link>
-          <Link className="NavBar-Link" to="/modificare">
-            Modificare
-          </Link>
-          <Link className="NavBar-Link" to="/stergere">
-            Stergere
-          </Link>
-          <Link className="NavBar-Link" to="/interogare">
-            Interogare
-          </Link>
-        </Tabs>
+      <AppBar className="NavBar-AppBar" position="static">
+        <Link className="NavBar-Link" to="/programare" value="/programare">
+          Programare
+        </Link>
+        <Link className="NavBar-Link" to="/modificare" value="/modificare">
+          Modificare
+        </Link>
+        <Link className="NavBar-Link" to="/stergere">
+          Stergere
+        </Link>
+        <Link className="NavBar-Link" to="/interogare">
+          Interogare
+        </Link>
       </AppBar>
     </div>
   );
