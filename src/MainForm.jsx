c1ebd54bd5e9b8302961e15f2992184aad9cc568
Constantin Alexandru <UnCorrupted_00@protonmail.ch>
2020-04-28 13:29:52 +0300

    converted fields in material-ui components

--- src/MainForm.jsx
+++ src/MainForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./MainForm.css";
 import TextField from "@material-ui/core/TextField";
+import NativeSelect from "@material-ui/core/NativeSelect";
 
 class MainForm extends Component {
   constructor() {
@@ -16,26 +17,30 @@ class MainForm extends Component {
         <h1>Programare Examen</h1>
         <form>
           <label for="specializare">Specializare</label>
-          <select className="MainForm-Field" id="sesiune">
+          <NativeSelect className="MainForm-Field" id="specializare">
             <option>Informatica-Economica</option>
             <option>Informatica-Matematica</option>
-          </select>
+          </NativeSelect>
           <label for="materia">Materia</label>
-          <select className="MainForm-Field" id="materia">
+          <NativeSelect className="MainForm-Field" id="materia">
             <option>C#</option>
             <option>Java</option>
             <option selected>Web Design</option>
             <option>Baze de Date SQL</option>
             <option>Algebra Liniara</option>
             <option>Econometrie</option>
-          </select>
+          </NativeSelect>
           <label for="profesor">Nume Profesor</label>
-          <input className="MainForm-Field" type="text" id="profesor" />
+          <TextField
+            style={{ marginBottom: "20px" }}
+            type="text"
+            id="profesor"
+          />
           <label for="sesiune">Perioada Sesiune</label>
-          <select className="MainForm-Field" id="sesiune">
+          <NativeSelect className="MainForm-Field" id="sesiune">
             <option>Vara</option>
             <option>Iarna</option>
-          </select>
+          </NativeSelect>
           <TextField
             style={{ marginBottom: "20px" }}
             label="An Universitar"
@@ -54,11 +59,7 @@ class MainForm extends Component {
             type="date"
             defaultValue="2019-05-24"
           />
-          <TextField
-            label="Ora examenului"
-            type="time"
-            defaultValue="07:30"
-          />
+          <TextField label="Ora examenului" type="time" defaultValue="07:30" />
         </form>
       </div>
     );
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,40 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import "./NavBar.css";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
 import { Link } from "react-router-dom";
 
-class NavBar extends Component {
-  constructor() {
-    super();
-    this.handleNavigation = this.handleNavigation.bind(this);
-  }
-
-  handleNavigation() {}
-
-  render() {
-    return (
-      <div className="NavBar">
-        <AppBar position="static">
-          <Tabs centered>
-            <Link className="NavBar-Link" to="/programare">
-              Programare
-            </Link>
-            <Link className="NavBar-Link" to="/modificare">
-              Modificare
-            </Link>
-            <Link className="NavBar-Link" to="/stergere">
-              Stergere
-            </Link>
-            <Link className="NavBar-Link" to="/interogare">
-              Interogare
-            </Link>
-          </Tabs>
-        </AppBar>
-      </div>
-    );
-  }
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <AppBar position="static">
+        <Tabs centered>
+          <Link className="NavBar-Link" to="/programare">
+            Programare
+          </Link>
+          <Link className="NavBar-Link" to="/modificare">
+            Modificare
+          </Link>
+          <Link className="NavBar-Link" to="/stergere">
+            Stergere
+          </Link>
+          <Link className="NavBar-Link" to="/interogare">
+            Interogare
+          </Link>
+        </Tabs>
+      </AppBar>
+    </div>
+  );
 }
 
 export default NavBar;
