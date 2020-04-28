31d2517686b5893566cfe3ffa7f64ef1c868dfda
Constantin Alexandru <UnCorrupted_00@protonmail.ch>
2020-04-28 14:16:13 +0300

    add state to Form component to keep exam schedule details

--- src/MainForm.jsx
+++ src/MainForm.jsx
@@ -2,42 +2,82 @@ import React, { Component } from "react";
 import "./MainForm.css";
 import TextField from "@material-ui/core/TextField";
 import NativeSelect from "@material-ui/core/NativeSelect";
+import Button from "@material-ui/core/Button";
 
 class MainForm extends Component {
   constructor() {
     super();
     this.state = {
-      materia: "",
+      specializare: "",
+      materie: "",
       profesor: "",
+      sesiune: "",
+      an_univ: 2019,
+      an_stud: 1,
+      data_exam: "2019-05-24",
+      ora_exam: "07:30",
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleSubmit(ev) {
+    ev.preventDefault();
+    console.log(this.state);
+  }
+
+  handleChange(ev) {
+    // console.log(ev.target.value);
+    this.setState({
+      [ev.target.name]: ev.target.value,
+    });
+  }
+
   render() {
     return (
       <div className="MainForm">
         <h1>Programare Examen</h1>
-        <form>
-          <label for="specializare">Specializare</label>
-          <NativeSelect className="MainForm-Field" id="specializare">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="specializare">Specializare</label>
+          <NativeSelect
+            className="MainForm-Field"
+            id="specializare"
+            onChange={this.handleChange}
+            name="specializare"
+          >
             <option>Informatica-Economica</option>
             <option>Informatica-Matematica</option>
           </NativeSelect>
-          <label for="materia">Materia</label>
-          <NativeSelect className="MainForm-Field" id="materia">
+          <label htmlFor="materia">Materia</label>
+          <NativeSelect
+            className="MainForm-Field"
+            id="materia"
+            onChange={this.handleChange}
+            name="materie"
+          >
             <option>C#</option>
             <option>Java</option>
-            <option selected>Web Design</option>
+            <option defaultValue>Web Design</option>
             <option>Baze de Date SQL</option>
             <option>Algebra Liniara</option>
             <option>Econometrie</option>
           </NativeSelect>
-          <label for="profesor">Nume Profesor</label>
+          <label htmlFor="profesor">Nume Profesor</label>
           <TextField
             style={{ marginBottom: "20px" }}
             type="text"
             id="profesor"
+            name="profesor"
+            value={this.state.profesor}
+            onChange={this.handleChange}
           />
-          <label for="sesiune">Perioada Sesiune</label>
-          <NativeSelect className="MainForm-Field" id="sesiune">
+          <label htmlFor="sesiune">Perioada Sesiune</label>
+          <NativeSelect
+            className="MainForm-Field"
+            id="sesiune"
+            onChange={this.handleChange}
+            name="sesiune"
+          >
             <option>Vara</option>
             <option>Iarna</option>
           </NativeSelect>
@@ -45,21 +85,42 @@ class MainForm extends Component {
             style={{ marginBottom: "20px" }}
             label="An Universitar"
             type="number"
-            defaultValue="2019"
+            name="an_univ"
+            value={this.state.an_univ}
+            onChange={this.handleChange}
           />
           <TextField
             style={{ marginBottom: "20px" }}
             label="An de studii"
             type="number"
-            defaultValue="1"
+            name="an_stud"
+            value={this.state.an_stud}
+            onChange={this.handleChange}
           />
           <TextField
             style={{ marginBottom: "20px" }}
             label="Data examenului"
             type="date"
-            defaultValue="2019-05-24"
+            name="data_exam"
+            value={this.state.data_exam}
+            onChange={this.handleChange}
           />
-          <TextField label="Ora examenului" type="time" defaultValue="07:30" />
+          <TextField
+            style={{ marginBottom: "20px" }}
+            label="Ora examenului"
+            type="time"
+            name="ora_exam"
+            value={this.state.ora_exam}
+            onChange={this.handleChange}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disableElevation
+          >
+            Planifica Examenul
+          </Button>
         </form>
       </div>
     );
