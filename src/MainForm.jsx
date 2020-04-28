33a2283df03510e345722999dec1e4d88aef75d2
Constantin Alexandru <UnCorrupted_00@protonmail.ch>
2020-04-28 13:02:03 +0300

    applied margin for input fields

--- src/MainForm.jsx
+++ src/MainForm.jsx
@@ -16,12 +16,12 @@ class MainForm extends Component {
         <h1>Programare Examen</h1>
         <form>
           <label for="specializare">Specializare</label>
-          <select id="sesiune">
+          <select className="MainForm-Field" id="sesiune">
             <option>Informatica-Economica</option>
             <option>Informatica-Matematica</option>
           </select>
           <label for="materia">Materia</label>
-          <select id="materia">
+          <select className="MainForm-Field" id="materia">
             <option>C#</option>
             <option>Java</option>
             <option selected>Web Design</option>
@@ -30,20 +30,35 @@ class MainForm extends Component {
             <option>Econometrie</option>
           </select>
           <label for="profesor">Nume Profesor</label>
-          <input type="text" id="profesor" />
+          <input className="MainForm-Field" type="text" id="profesor" />
           <label for="sesiune">Perioada Sesiune</label>
-          <select id="sesiune">
+          <select className="MainForm-Field" id="sesiune">
             <option>Vara</option>
             <option>Iarna</option>
           </select>
-          <TextField label="An Universitar" type="number" defaultValue="2019" />
-          <TextField label="An de studii" type="number" defaultValue="1" />
           <TextField
+            style={{ marginBottom: "20px" }}
+            label="An Universitar"
+            type="number"
+            defaultValue="2019"
+          />
+          <TextField
+            style={{ marginBottom: "20px" }}
+            label="An de studii"
+            type="number"
+            defaultValue="1"
+          />
+          <TextField
+            style={{ marginBottom: "20px" }}
             label="Data examenului"
             type="date"
             defaultValue="2019-05-24"
           />
-          <TextField label="Ora examenului" type="time" defaultValue="07:30" />
+          <TextField
+            label="Ora examenului"
+            type="time"
+            defaultValue="07:30"
+          />
         </form>
       </div>
     );
