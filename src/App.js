af629ac6dcb4e664a52bc2ebbfe90b28dcaba2b3
Constantin Alexandru <UnCorrupted_00@protonmail.ch>
2020-04-28 11:55:03 +0300

    created form with inputs for  exam details

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import Container from "@material-ui/core/Container";
 import NavBar from './NavBar';
+import MainForm from './MainForm';
 
 function App() {
   return (
     <Container className="App" maxWidth="md">
       <NavBar />
-      
+      <MainForm />
     </Container>
   );
 }
--- src/MainForm.jsx
+++ src/MainForm.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
+import "./MainForm.css";
+import TextField from "@material-ui/core/TextField";
 
 class MainForm extends Component {
   render() {
     return (
-      <div>
-        <h1>jfaoi</h1>
+      <div className="MainForm">
+        <h1>Programare Examen</h1>
+        <form>
+          <label for="materia">Materia</label>
+          <input type="text" name="meteria" />
+          <label for="profesor">Profesor</label>
+          <input type="text" name="profesor" />
+          <label for="sesiune">Sesiunea</label>
+          <input type="text" name="sesiune" />
+          <TextField label="An Universitar" type="number" defaultValue="2019" />
+          <TextField label="An de studii" type="number" defaultValue="1" />
+          <label for="sectia">Sectia</label>
+          <input type="text" name="sectia" />
+          <TextField
+            label="Data examenului"
+            type="date"
+            defaultValue="2019-05-24"
+          />
+          <TextField label="Ora examenului" type="time" defaultValue="07:30" />
+        </form>
       </div>
     );
   }
