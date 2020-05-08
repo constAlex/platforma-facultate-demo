9778a3824f5b3623602d731e267ca282161c1178
AlexandruCon <alexandru.constantin@ibm.com>
2020-05-08 18:01:10 +0300

    accordion material-ui element refactoring

--- src/Main.jsx
+++ src/Main.jsx
@@ -7,6 +7,52 @@ import ListItemText from "@material-ui/core/ListItemText";
 import Collapse from "@material-ui/core/Collapse";
 import ExpandLess from "@material-ui/icons/ExpandLess";
 import ExpandMore from "@material-ui/icons/ExpandMore";
+import { withStyles } from '@material-ui/core/styles';
+import MuiExpansionPanel from '@material-ui/core/ExpansionPanel';
+import MuiExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import MuiExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+
+const ExpansionPanel = withStyles({
+  root: {
+    border: '1px solid rgba(0, 0, 0, .125)',
+    boxShadow: 'none',
+    '&:not(:last-child)': {
+      borderBottom: 0,
+    },
+    '&:before': {
+      display: 'none',
+    },
+    '&$expanded': {
+      margin: 'auto',
+    },
+  },
+  expanded: {},
+})(MuiExpansionPanel);
+
+const ExpansionPanelSummary = withStyles({
+  root: {
+    backgroundColor: 'rgba(0, 0, 0, .03)',
+    borderBottom: '1px solid rgba(0, 0, 0, .125)',
+    marginBottom: -1,
+    minHeight: 56,
+    '&$expanded': {
+      minHeight: 56,
+    },
+  },
+  content: {
+    '&$expanded': {
+      margin: '12px 0',
+    },
+  },
+  expanded: {},
+})(MuiExpansionPanelSummary);
+
+const ExpansionPanelDetails = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiExpansionPanelDetails);
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,10 +67,10 @@ const useStyles = makeStyles((theme) => ({
 
 function Main({ programari }) {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState('0');
 
-  const handleClick = () => {
-    setOpen(!open);
+  const handleClick = (id) => (event, newExpanded) => {
+    setOpen(newExpanded ? id : false);
   };
   return (
     <List
@@ -32,19 +78,31 @@ function Main({ programari }) {
       aria-labelledby="nested-list-subheader"
       subheader={
         <ListSubheader component="div" id="nested-list-subheader">
-          Programari Active
+          Programari Actived
         </ListSubheader>
       }
       className={classes.root}
     >
       <div>
-        {programari.map((programare) => (
+        {programari.map((programare, index) => (
           <div style={{border: "1px solid black", marginBottom: "5px"}} key={programare.id}>
-            <ListItem button onClick={handleClick}>
-              <ListItemText primary={programare.materie} />
-              {open ? <ExpandLess /> : <ExpandMore />}
-            </ListItem>
-            <Collapse in={open} timeout="auto" unmountOnExit>
+            <ExpansionPanel square expanded={open === programare.id} onChange={handleClick(programare.id)}>
+        <ExpansionPanelSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography>{programare.materie}</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+        <Typography>
+        <ListItem>Specializarea: {programare.specializare}</ListItem>
+              <ListItem>Profesor: {programare.profesor}</ListItem>
+              <ListItem>Sesiune: {programare.sesiune}</ListItem>
+              <ListItem>Anul: {programare.an_univ}</ListItem>
+              <ListItem>An Studii: {programare.an_stud}</ListItem>
+              <ListItem>Data Examen: {programare.data_exam}</ListItem>
+              <ListItem>Ora Examen: {programare.ora_exam}</ListItem>
+        </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+            {/* <Collapse in={open === programare.id} timeout="auto" unmountOnExit>
               <ListItem>Specializarea: {programare.specializare}</ListItem>
               <ListItem>Profesor: {programare.profesor}</ListItem>
               <ListItem>Sesiune: {programare.sesiune}</ListItem>
@@ -52,7 +110,7 @@ function Main({ programari }) {
               <ListItem>An Studii: {programare.an_stud}</ListItem>
               <ListItem>Data Examen: {programare.data_exam}</ListItem>
               <ListItem>Ora Examen: {programare.ora_exam}</ListItem>
-            </Collapse>
+            </Collapse> */}
           </div>
         ))}
       </div>
