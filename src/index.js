2d4f01e9fe0a31bd9a6007e5b294dd1880b083dc
AlexandruCon <alexandru.constantin@ibm.com>
2021-01-11 15:03:28 +0200

    added login template component from material-ui

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { Provider} from "react-redux";
+import IFrameDemo from "./components/IFrameDemo/IFrameDemo";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/login" render={() => <IFrameDemo />} />
+        <App />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
